9a46e55808a01bc1ff7df028f167df15e60ec79d
Murali <murali.24.4.91@gmail.com>
2018-05-19 23:16:17 +0530

    Filter implementation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -151,6 +151,7 @@ export class HomePage {
       this.events = this.events.filter(ev => {
         return (ev.title.indexOf(data.title) > -1);
       });
+      //this.setEvents(start, end);
     });
   }
 
